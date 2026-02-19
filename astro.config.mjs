65c46e053c2fdf2af8c02a3b322b82ed8dee1179
MeloDev <melodiaz2111@gmail.com>
2026-02-19 12:16:16 -0500

    config: add base path config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: "https://MeloDev2111.github.io",
+    base: "/Chamba-Lab",
     integrations: [react()],
 
     vite: {
